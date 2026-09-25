fd8a22056da0a98fddf28f55a7b0ea8efcccab72
Isaias-Berdin <berdin.ij@obmontessori.edu.ph>
2026-09-25 20:43:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,27 @@ def create_order(e):
     addon2 = document.getElementById("addon2")
     addon3 = document.getElementById("addon3")
 
+     # elements for bar menu
+    bar1 = document.getElementById("bar1")
+    bar2 = document.getElementById("bar2")
+    bar3 = document.getElementById("bar3")
+
+
     # calculates the subtotal using boolean multiplication
-    subtotal = (float(prod1.value) * prod1.checked +
-                float(prod2.value) * prod2.checked +
-                float(prod3.value) * prod3.checked +
-                float(prod4.value) * prod4.checked +
-                float(prod5.value) * prod5.checked +
-                float(regular.value) * regular.checked +
-                float(large.value) * large.checked +
-                float(addon1.value) * addon1.checked +
-                float(addon2.value) * addon2.checked +
-                float(addon3.value) * addon3.checked)
+    subtotal = (float(prod1.value) * prod1.checked
+    + float(prod2.value) * prod2.checked +
+    float(prod3.value) * prod3.checked +
+    float(prod4.value) * prod4.checked +    
+    float(prod5.value) * prod5.checked +
+    float(regular.value) * regular.checked +
+    float(large.value) * large.checked +
+    float(addon1.value) * addon1.checked +
+    float(addon2.value) * addon2.checked +
+    float(addon3.value) * addon3.checked +
+    float(bar1.value) * bar1.checked + 
+    float(bar2.value) * bar2.checked + 
+    float(bar3.value) * bar3.checked)
+    
 
     # The tax calculation
     tax_rate = 0.12  # 12% because its the standard
